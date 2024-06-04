450a4488085c553ea800f54dc2b02eed809e5cca
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2024-06-04 21:45:13 +0100

    handling responses and error

--- src/wallet-address/wallet-address.controller.ts
+++ src/wallet-address/wallet-address.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete, NotFoundException, InternalServerErrorException } from '@nestjs/common';
 import { WalletAddressService } from './wallet-address.service';
 import { CreateWalletAddressDto } from './dto/create-wallet-address.dto';
 
@@ -7,27 +7,78 @@ export class WalletAddressController {
     constructor(private readonly walletAddressService: WalletAddressService) { }
 
     @Get()
-    findAll() {
-        return this.walletAddressService.findAll();
+    async findAll() {
+        try {
+            let data = await this.walletAddressService.findAll();
+            return {
+                message: 'Request successful',
+                status: "success",
+                data: data
+            }
+        } catch (e) {
+            throw new NotFoundException()
+        }
     }
 
     @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.walletAddressService.findOne(+id);
+    async findOne(@Param('id') id: string) {
+        try {
+            let wallet = await this.walletAddressService.findOne(+id);
+            return {
+                message: 'Wallet details fetched successfully',
+                status: "success",
+                data: wallet
+            }
+        } catch (e) {
+            throw new NotFoundException()
+        }
     }
 
     @Post()
-    create(@Body() createWalletAddressDto: CreateWalletAddressDto) {
-        return this.walletAddressService.create(createWalletAddressDto);
+    async create(@Body() createWalletAddressDto: CreateWalletAddressDto) {
+        try {
+            let created_wallet = await this.walletAddressService.create(createWalletAddressDto);
+            return {
+                message: 'Wallet created successfully',
+                status: "success",
+                data: created_wallet
+            }
+
+        } catch (e) {
+            throw new InternalServerErrorException()
+        }
     }
 
     @Put(':id')
-    update(@Param('id') id: string, @Body() updateWalletAddressDto: CreateWalletAddressDto) {
-        return this.walletAddressService.update(+id, updateWalletAddressDto);
+    async update(@Param('id') id: string, @Body() updateWalletAddressDto: CreateWalletAddressDto) {
+
+        try {
+            let wallet = await this.walletAddressService.update(+id, updateWalletAddressDto);
+
+            return {
+                message: 'Wallet Updated successfully',
+                status: "success",
+                data: wallet
+            }
+
+        } catch (e) {
+            throw new InternalServerErrorException("Error updating wallet", e.message)
+        }
+
     }
 
     @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.walletAddressService.remove(+id);
+    async remove(@Param('id') id: string) {
+
+        try {
+            await this.walletAddressService.remove(+id);
+
+            return {
+                message: 'Wallet deletedsuccessfully',
+                status: "success",
+            }
+        } catch (e) {
+            throw new InternalServerErrorException()
+        }
     }
 }
